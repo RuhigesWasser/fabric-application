8b0da8d61c4c32acf92c9c43bea862d279e20795
RuhigesW <ruhigeswl@outlook.com>
2024-04-10 17:21:25 +0800

    事件监听

--- src/main/java/com/example/application/controller/FabricController.java
+++ src/main/java/com/example/application/controller/FabricController.java
@@ -1,12 +1,18 @@
 package com.example.application.controller;
 
+import com.example.application.entity.Courier;
+import com.example.application.entity.Member;
+import com.example.application.entity.Production;
 import com.google.gson.Gson;
 import org.hyperledger.fabric.client.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class FabricController {
@@ -59,6 +65,105 @@ public class FabricController {
         }
     }
 
+    @PostMapping(value = "/updateMember/{type}")
+    public String updateMember(@PathVariable String type, @RequestParam Map<String, Object> map) throws Exception {
+        Member member = new Member();
+        Gson gson = new Gson();
+
+        member.setId((String) map.get("id"));
+        member.setAddress((String) map.get("address"));
+        member.setName((String) map.get("name"));
+        member.setEmail((String) map.get("email"));
+        member.setPhone((String) map.get("phone"));
+        String memberSort = gson.toJson(member);
+
+        type = "updateCommodity" + type;
+
+        String traceability = (String) map.get("traceability");
+
+        try {
+            contract.submitTransaction(type,traceability,memberSort);
+            return "成功更新" + traceability + "的信息";
+        } catch (EndorseException | CommitException | SubmitException | CommitStatusException e) {
+            return e.getMessage();
+        }
+    }
+
+    @PostMapping(value = "/updateCourier")
+    public String updateCourier(@RequestParam Map<String, Object> map) throws Exception {
+        Courier courier = new Courier();
+        Gson gson = new Gson();
+
+        courier.setCourierId((String) map.get("courierId"));
+        courier.setCarrierName((String) map.get("carrierName"));
+        courier.setCarrierCompany((String) map.get("carrierCompany"));
+        courier.setCarrierPhone((String) map.get("carrierPhone"));
+        courier.setStartTime((String) map.get("startTime"));
+        courier.setEndTime((String) map.get("endTime"));
+        courier.setPosition((String) map.get("position"));
+        String courierSort = gson.toJson(courier);
+
+        String traceability = (String) map.get("traceability");
+
+        try {
+            contract.submitTransaction("updateCommodityCourier",traceability,courierSort);
+            return "成功更新" + traceability + "的物流信息";
+        } catch (EndorseException | CommitException | SubmitException | CommitStatusException e) {
+            return e.getMessage();
+        }
+    }
+
+    @PostMapping(value = "/updateAuthority")
+    public String updateAuthority(@RequestParam Map<String, Object> map) throws Exception {
+
+        String traceability = (String) map.get("traceability");
+
+        try {
+            contract.submitTransaction("updateCommodityAuthority",traceability,
+                    (String) map.get("barCode"),
+                    (String) map.get("name"),
+                    (String) map.get("category"),
+                    (String) map.get("brand"));
+            return "成功更新" + traceability + "的识别信息";
+        } catch (EndorseException | CommitException | SubmitException | CommitStatusException e) {
+            return e.getMessage();
+        }
+    }
+
+//    更新生产环节信息，监听链码事件
+    @PostMapping(value = "/updateProduction")
+    public String updateProduction(@RequestParam Map<String, Object> map) throws Exception {
+
+        Production production = new Production();
+        Gson gson = new Gson();
+
+        String traceability = (String) map.get("traceability");
+
+        production.setTemperature((String) map.get("temperature"));
+        production.setAirHumidity((String) map.get("airHumidity"));
+        production.setSoilHumidity((String) map.get("soilHumidity"));
+        production.setWind((String) map.get("wind"));
+        production.setSunlight((String) map.get("sunLight"));
+        production.setDisaster((String) map.get("disaster"));
+        production.setIsInsured((String) map.get("isInsured"));
+        production.setIsCompensated((String) map.get("isCompensated"));
+        String productionSort = gson.toJson(production);
+
+        try (var eventSession = startChaincodeEventListening()) {
+            var commit = contract.newProposal("updateCommodityProduction")
+                    .addArguments(traceability, productionSort)
+                    .build()
+                    .endorse()
+                    .submitAsync();
+            var status = commit.getStatus();
+            if (!status.isSuccessful()) {
+                throw new RuntimeException("failed to commit transaction with status code " + status.getCode());
+            }
+            return "成功更新生产信息";
+        }
+
+    }
+
 //    获取全部溯源码
     @GetMapping(value = "/queryAllKey")
     public String queryAllKey() throws Exception {
@@ -104,6 +209,50 @@ public class FabricController {
         } catch (GatewayException e) {
             System.out.println(e.getMessage());
             return e.getMessage();
+        }
+    }
+
+//    测试，模拟商品投保
+    @GetMapping(value = "/insure")
+    public String insure(@RequestParam String traceability) throws Exception {
+        try {
+            contract.submitTransaction("setInsurance",traceability);
+            return "投保成功！";
+        } catch (EndorseException | CommitException | SubmitException | CommitStatusException e) {
+            return e.getMessage();
+        }
+    }
+
+    private CloseableIterator<ChaincodeEvent> startChaincodeEventListening() {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        var eventIter = gateway.getNetwork(System.getenv().getOrDefault("CHANNEL_NAME", "mychannel")).getChaincodeEvents(System.getenv().getOrDefault("CHAINCODE_NAME", "basic"));
+        executor.execute(() -> readEvents(eventIter));
+
+        return eventIter;
+    }
+
+    private void readEvents(final CloseableIterator<ChaincodeEvent> eventIter) {
+        try {
+            eventIter.forEachRemaining(event -> {
+                String payload = new String(event.getPayload());
+                if(event.getEventName().equals("disaster")) {
+                    Pattern pattern = Pattern.compile("commodity.*?Z");
+                    Matcher matcher = pattern.matcher(payload);
+                    if(matcher.find())
+                    {
+                        payload = matcher.group();
+                    }
+                    try {
+                        contract.submitTransaction("setCompensation",payload);
+                    } catch (EndorseException | SubmitException | CommitStatusException | CommitException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        } catch (GatewayRuntimeException e) {
+            throw e;
         }
     }
 }
